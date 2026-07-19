8de969ece16088faa1d551bed15d892c7a9dfc3a
Shubham Kaushik <205813693+i-ShubhamKaushik@users.noreply.github.com>
2026-07-19 09:53:58 +0530

    Virtual Environment

---
+++ Virtual environment/virtualenv.py
@@ -0,0 +1,12 @@
+# Few Useful Commands of Virtual Environment
+
+# python -m venv env
+# .\env\Scripts\Activate.ps1
+# deactivate
+# pip install package_name
+# pip uninstall package_name
+# pip list
+# pip show package_name
+# pip freeze > requirements.txt
+# pip install -r requirements.txt
+# python -m pip install --upgrade pip
